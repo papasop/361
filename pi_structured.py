0694dc717574d6d983c9563cfbbb244a591f79ef
papasop <papaso@icloud.com>
2025-07-14 13:43:36 +1200

    Update pi_structured.py

--- pi_structured.py
+++ pi_structured.py
@@ -8,35 +8,32 @@ Implements a hybrid approximation of π:
 This improves convergence compared to the pure Leibniz series.
 """
 
-from mpmath import mp
+from mpmath import mp, atan, mpf, pi, fsum
+from itertools import product
 
-# Set desired precision
-mp.dps = 50  # decimal digits
+mp.dps = 50  # 设置精度
 
-# Machin-like correction φ = 4·arctan(1/5) − arctan(1/239)
-a_k = [4, -1]
-b_k = [1, 1]
-c_k = [5, 239]
-
-def compute_phi():
-    """Fixed modal correction φ(n), independent of n"""
-    return mp.fsum([mp.mpf(a_k[j]) * mp.atan(mp.mpf(b_k[j]) / mp.mpf(c_k[j]))
-                    for j in range(len(a_k))])
-
-def leibniz_sum(n):
-    """Partial sum of Leibniz series with n terms"""
-    return mp.fsum([mp.mpf(4) * (-1)**k / (2 * k + 1) for k in range(n)])
-
-def pi_structured(n=1000):
+def generate_phi_candidates(N=2, max_den=300, max_coeff=20, tol=1e-10):
     """
-    Structured approximation of π:
-        ρ(n) = Leibniz_sum(n) + φ
-
-    Parameters:
-        n (int): number of Leibniz terms
-
-    Returns:
-        mpf: approximate value of π
+    自动搜索 φ(n) 的组合，使其逼近 π，返回最优残差组合。
     """
-    return leibniz_sum(n) + compute_phi()
+    best_phi = None
+    best_residual = mpf('inf')
+    best_combo = None
 
+    for coeffs in product(range(1, max_coeff+1), repeat=N):
+        for denoms in product(range(1, max_den+1), repeat=N):
+            a_k = coeffs
+            c_k = denoms
+            try:
+                phi = fsum([mpf(a) * atan(mpf(1)/mpf(c)) for a, c in zip(a_k, c_k)])
+                residual = abs(pi - phi)
+                if residual < best_residual:
+                    best_phi = phi
+                    best_combo = list(zip(a_k, [1]*N, c_k))  # a_k, b_k=1, c_k
+                    best_residual = residual
+                    if residual < tol:
+                        return best_phi, best_combo, residual
+            except:
+                continue
+    return best_phi, best_combo, best_residual
